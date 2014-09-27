7c03aebf86b49d5d884c3d697d03b9aff642be89
Star.yu <star.yu@toursforfun.com>
2014-09-27 16:28:25 +0800

    pick

--- wordtree.py
+++ wordtree.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin python
 # -*- coding: utf-8 -*-
-import sys,ujson
+import sys,gzip
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -8,6 +13,7 @@ class wordtree(object):
 
     tree = {}
     index_depath = 0
+    coredict = './dict/core.dict'
     @staticmethod
     def insert(in_word, in_speechtagging):
         in_word = in_word.decode("utf-8")
@@ -17,16 +23,15 @@ class wordtree(object):
     @staticmethod
     def createstems(in_word, __stems, in_speechtagging):
         count = len(in_word)
-        char = str(ord(in_word[0]))
-        if count == 1:
-            if not __stems.has_key(char):
-                __stems[char] = {}
-            if not __stems.has_key('k'):
-                __stems['k'] = {}
-            __stems['k'][char] = in_speechtagging
-            return True
+        char = ord(in_word[0])
         if not __stems.has_key(char):
-            __stems.update({char: {}, 'k': {}})
+            __stems[char] = {}
+        if not __stems.has_key('k'):
+            __stems['k'] = {}
+        __stems['k'][char] = in_speechtagging
+
+        if count == 1:
+            return True
         return wordtree.createstems(in_word[1:count], __stems.get(char), in_speechtagging)
 
     @staticmethod
@@ -40,13 +45,27 @@ class wordtree(object):
         if __tree == None:
             return {'status': False}
         count = len(in_word)
-        char = str(ord(in_word[0]))
+        char = ord(in_word[0])
         if count == 1:
             if __tree.has_key(char):
                 __tagging = __tree.get('k').get(char)
-                # if __tagging != None:
-                return {'s': __tagging, 'status': True}
+                if __tagging != None:
+                    return {'s': __tagging, 'status': True}
             return {'status': False}
         wordtree.index_depath +=1
         return wordtree.depathcheck(in_word[1:count], __tree.get(char))
 
+    @staticmethod
+    def picksave():
+        for line in open('words.txt'):
+            wordsinfo = line.strip('\n').split('	')
+            wordtree.insert(wordsinfo[0], wordsinfo[1])
+        gfile = gzip.open(wordtree.coredict, 'wb')
+        pickle.dump(wordtree.tree, gfile)
+        gfile.close()
+
+    @staticmethod
+    def pickload():
+        gfile = gzip.open(wordtree.coredict, 'rb')
+        wordtree.tree = pickle.load(gfile)
+        gfile.close()
