5db487d5c214f47cc3ea2489ab8f2c7fff6e44a2
Star.yu <star.yu@toursforfun.com>
2014-10-05 01:42:30 +0800

    new method

--- dictionary.py
+++ dictionary.py
@@ -7,34 +7,34 @@ sys.setdefaultencoding('utf8')
 
 class dictionary (object):
 
-    """
-        dictionary
-        """
     symbol = {}
 
-    """
+    '''
         load default dictionary
-        """
+        '''
     @staticmethod
     def loadcoredict():
         wordtree.pickload()
         pass
 
+    '''
+        load custom dictionary
+        '''
     @staticmethod
     def loadcustomdict():
-       for line in open ("./dict/custom.dict"):
-        wordinfo = line.strip("\n").split("	")
-        wordtree.insert(wordinfo[0],wordinfo[1])
+        for line in open ("./dict/custom.dict"):
+            wordinfo = line.strip("\n").split("	")
+            wordtree.insert(wordinfo[0],wordinfo[1])
         pass
 
     @staticmethod
     def loadsymbolict():
-       for line in open ("./dict/symbol.dict"):
-        wordlist = line.split(" ")
-        len_list = len(wordlist)
-        for i in range(0,len_list,2):
-            if i+1 < len_list:
-                dictionary.symbol[long(wordlist[i])] = wordlist[i+1]
+        for line in open ("./dict/symbol.dict"):
+            wordlist = line.split(" ")
+            len_list = len(wordlist)
+            for i in range(0,len_list,2):
+                if i+1 < len_list:
+                    dictionary.symbol[long(wordlist[i])] = wordlist[i+1]
         pass
 
     @staticmethod
--- tests.py
+++ tests.py
@@ -3,4 +3,9 @@
 from act import act
 
 act = act()
-print act.segment("中华人",True)
+# print act.segment('年来 中希贸易始终处于较低的水平 希腊几乎没有在中国投资')
+output = open('./seg_word_p.txt', 'aw+')
+for line in open('./msr/msr_test'):
+    line = line.strip('\n')
+    output.write(act.segment(line, False)+'\n')
+output.close()
